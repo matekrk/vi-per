35cbb1e98c729536a114bb82c998860431a45559
MateuszP <mateusz.pyla@doctoral.uj.edu.pl>
2025-04-23 16:17:51 +0200

    udpdate configs and command (extend with vblls)

---
+++ newcode/vbll_diag_cc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=newcode
+#SBATCH --qos=normal
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=rtx2080
+#SBATCH --output=out_slurms/vbll_diag_cc_%j.out
+
+
+cd /home/pyla/bayesian/from_notebook/vi-per
+
+eval "$(conda shell.bash hook)"
+conda activate bfn2
+
+python src/main.py --config newcode/vbll_diag_cc.json
